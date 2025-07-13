848a7320776f5e587255d1e751618d171280fb5c
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-07-13 19:40:06 +0530

    added bit of api

--- app/pokemon/page.js
+++ app/pokemon/page.js
@@ -1,15 +1,24 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-const page = () => {
+const Pokemon = () => {
 
     const searchParams = useSearchParams();
     const name = searchParams.get('name');
 
-  return (
-    <div>{name}</div>
-  )
+    useEffect(() => {
+        async function fetchPokemon() {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+            const result = await response.json();
+            console.log(result)
+        }
+        fetchPokemon() 
+    })
+
+    return (
+        <div>{name}</div>
+    )
 }
 
-export default page
+export default Pokemon
